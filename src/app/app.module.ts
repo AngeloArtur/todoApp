5342688ef8541d17a7f23a770e3a9b32fef053ff
Ângelo Artur <ang.vianna@gmail.com>
2024-08-11 10:16:17 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { SidebarModule } from 'primeng/sidebar';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { environment } from '../environment/environment';
+import { SidebarComponent } from './core/components/sidebar/sidebar.component';
+import { HomeComponent } from './feature/home/home.component';
+
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SidebarComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    SidebarModule,
   ],
-  providers: [
-    provideClientHydration()
-  ],
-  bootstrap: [AppComponent]
+  providers: [provideClientHydration()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/environment/environment.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBGTOameYiG0UaqFGH3VnNMmbxrIEsOqls',
+    authDomain: 'todoapp-dbeda.firebaseapp.com',
+    projectId: 'todoapp-dbeda',
+    storageBucket: 'todoapp-dbeda.appspot.com',
+    messagingSenderId: '202337708163',
+    appId: '1:202337708163:web:212034cbf48cb6a11bd3b6',
+    measurementId: 'G-BKNJDNZ27T',
+  }
+};
+
+// Initialize Firebase
