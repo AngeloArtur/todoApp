0a195e30bf3f899bb589083c1c61c066c0587527
Ângelo Artur <ang.vianna@gmail.com>
2024-08-11 23:25:28 -0300

    fix: dilog function

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../../shared/services/modal.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
   date = new Date();
+
+  constructor(private modalService: ModalService) {}
+
+  openDialog(collection: string) {
+    this.modalService.setCollectionName(collection)
+    this.modalService.show();
+  }
 }
