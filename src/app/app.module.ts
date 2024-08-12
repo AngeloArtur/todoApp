ab78b559de3354116de06616168fa666a57e2993
Ângelo Artur <ang.vianna@gmail.com>
2024-08-11 23:26:33 -0300

    fix: imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SidebarModule } from 'primeng/sidebar';
+import { DialogModule } from 'primeng/dialog';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputTextModule } from 'primeng/inputtext';
+import { MenubarModule } from 'primeng/menubar';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
@@ -12,16 +20,26 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environment/environment';
 import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 import { HomeComponent } from './feature/home/home.component';
-
+import { NewTaskDialogComponent } from './core/components/new-task-dialog/new-task-dialog.component';
 
 @NgModule({
-  declarations: [AppComponent, SidebarComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    SidebarComponent,
+    HomeComponent,
+    NewTaskDialogComponent,
+  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
-    SidebarModule,
+    DialogModule,
+    InputMaskModule,
+    InputTextModule,
+    MenubarModule,
+    ReactiveFormsModule,
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
