92202c6f4a9d67571bf311037aeebbdc8531fc46
Ângelo Artur <ang.vianna@gmail.com>
2024-08-13 17:29:36 -0300

    imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,19 @@ import {
 } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import localeBr from '@angular/common/locales/pt';
+import { LOCALE_ID } from '@angular/core';
+registerLocaleData(localeBr, 'pt');
+import { DatePipe } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CalendarModule } from 'primeng/calendar';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
 import { DialogModule } from 'primeng/dialog';
+import { DialogService, DynamicDialogModule } from 'primeng/dynamicdialog';
 import { InputMaskModule } from 'primeng/inputmask';
 import { InputTextModule } from 'primeng/inputtext';
 import { MenubarModule } from 'primeng/menubar';
@@ -21,6 +29,7 @@ import { environment } from '../environment/environment';
 import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 import { HomeComponent } from './feature/home/home.component';
 import { NewTaskDialogComponent } from './core/components/new-task-dialog/new-task-dialog.component';
+import { DynamicDialogComponent } from './core/components/dynamic-dialog/dynamic-dialog.component';
 
 @NgModule({
   declarations: [
@@ -28,6 +37,7 @@ import { NewTaskDialogComponent } from './core/components/new-task-dialog/new-ta
     SidebarComponent,
     HomeComponent,
     NewTaskDialogComponent,
+    DynamicDialogComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,13 +45,21 @@ import { NewTaskDialogComponent } from './core/components/new-task-dialog/new-ta
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFirestoreModule,
+    ConfirmPopupModule,
+    CalendarModule,
     DialogModule,
+    DynamicDialogModule,
     InputMaskModule,
     InputTextModule,
     MenubarModule,
     ReactiveFormsModule,
   ],
-  providers: [provideClientHydration()],
+  providers: [
+    provideClientHydration(),
+    DatePipe,
+    { provide: LOCALE_ID, useValue: 'pt' },
+    DialogService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
